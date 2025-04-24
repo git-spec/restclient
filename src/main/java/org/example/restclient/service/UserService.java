e6c1c74bf4b36f5d69f23bdc4cd9c898c7fbe934
git-spec <inkyfischer@gmail.com>
2025-04-24 11:47:14 +0200

    feat: Add RestClient to UserService.

--- src/main/java/org/example/restclient/service/UserService.java
+++ src/main/java/org/example/restclient/service/UserService.java
@@ -1,8 +1,16 @@
 package org.example.restclient.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 
 @Service
 public class UserService {
+    RestClient restClient;
+
+    public UserService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder
+            .baseUrl("https://reqres.in/api")
+            .build();
+    }
 }
