f082f64944bfd0ee3b6ef9f2800dc0964ffe0c4e
git-spec <inkyfischer@gmail.com>
2025-04-24 11:49:21 +0200

    feat: Create addUser to UserService.

--- src/main/java/org/example/restclient/service/UserService.java
+++ src/main/java/org/example/restclient/service/UserService.java
@@ -1,5 +1,9 @@
 package org.example.restclient.service;
 
+import org.example.restclient.model.RequestUser;
+import org.example.restclient.model.ResponseUser;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -12,5 +16,15 @@ public class UserService {
         this.restClient = restClientBuilder
             .baseUrl("https://reqres.in/api")
             .build();
+    }
+
+    public ResponseUser addUser(RequestUser user) {
+        return restClient
+            .post()
+            .uri("/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(user)
+            .retrieve()
+            .body(ResponseUser.class);
     }
 }
