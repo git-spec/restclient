819e35fb8ea30db9e3f6fdc7971848f5a867f17d
git-spec <inkyfischer@gmail.com>
2025-04-24 11:53:27 +0200

    feat: Add service to UserController.

--- src/main/java/org/example/restclient/controller/UserController.java
+++ src/main/java/org/example/restclient/controller/UserController.java
@@ -1,8 +1,14 @@
 package org.example.restclient.controller;
 
+import org.example.restclient.service.UserService;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
 }
