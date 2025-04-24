586a3c93e8b5f0567945b927e003af22a28f23e5
git-spec <inkyfischer@gmail.com>
2025-04-24 12:09:29 +0200

    feat: Create addUser to UserController.

--- src/main/java/org/example/restclient/controller/UserController.java
+++ src/main/java/org/example/restclient/controller/UserController.java
@@ -1,14 +1,25 @@
 package org.example.restclient.controller;
 
+import org.example.restclient.model.RequestUser;
+import org.example.restclient.model.ResponseUser;
 import org.example.restclient.service.UserService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
     private final UserService service;
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @PostMapping("/user")
+    public ResponseUser addUser(RequestUser user) {
+        return service.addUser(user);
     }
 }
