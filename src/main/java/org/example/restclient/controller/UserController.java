8c9e52f0004fe72826a72feb7dbb8513d44706da
git-spec <inkyfischer@gmail.com>
2025-04-25 09:42:56 +0200

    feat: Create deleteUser in UserController and UserService.

--- src/main/java/org/example/restclient/controller/UserController.java
+++ src/main/java/org/example/restclient/controller/UserController.java
@@ -3,9 +3,12 @@ package org.example.restclient.controller;
 import org.example.restclient.model.RequestUser;
 import org.example.restclient.model.ResponseUser;
 import org.example.restclient.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,5 +24,10 @@ public class UserController {
     @PostMapping("/user")
     public ResponseUser addUser(RequestUser user) {
         return service.addUser(user);
+    }
+
+    @DeleteMapping("/users")
+    public ResponseEntity<Void> deleteUser(@RequestParam String page) {
+        return service.deleteUser(page);
     }
 }
--- src/main/java/org/example/restclient/service/UserService.java
+++ src/main/java/org/example/restclient/service/UserService.java
@@ -5,6 +5,7 @@ import org.example.restclient.model.ResponseUser;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -27,5 +28,13 @@ public class UserService {
             .body(user)
             .retrieve()
             .body(ResponseUser.class);
+    }
+
+    public ResponseEntity<Void> deleteUser(String page) {
+        return restClient
+            .delete()
+            .uri("/users?page={page}", page)
+            .retrieve()
+            .toBodilessEntity();
     }
 }
